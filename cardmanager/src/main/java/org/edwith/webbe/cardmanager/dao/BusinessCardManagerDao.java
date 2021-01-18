3e87ce0b36b35f07f5e883ad5dc84c643010a38c
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-18 16:45:10 +0900

    insert문 구현 동일한 전화번호 존재할시  insert 제한  exception 처리 완료

--- cardmanager/src/main/java/org/edwith/webbe/cardmanager/dao/BusinessCardManagerDao.java
+++ cardmanager/src/main/java/org/edwith/webbe/cardmanager/dao/BusinessCardManagerDao.java
@@ -2,58 +2,71 @@ package org.edwith.webbe.cardmanager.dao;
 
 import org.edwith.webbe.cardmanager.dto.BusinessCard;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class BusinessCardManagerDao {
 
-    private static String dburl = "jdbc:mysql://localhost:3306/connectdb";
-    private static String dbUser = "connectuser";
-    private static String dbpasswd = "connect123!@#";
+	private static String dburl = "jdbc:mysql://localhost:3306/connectdb";
+	private static String dbUser = "connectuser";
+	private static String dbpasswd = "connect123!@#";
 
-    public List<BusinessCard> searchBusinessCard(String keyword){
-        List<BusinessCard> businessCardList = new ArrayList<BusinessCard>();
+	public List<BusinessCard> searchBusinessCard(String keyword) {
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch(ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+		List<BusinessCard> businessCardList = new ArrayList<BusinessCard>();
 
-        String sql ="SELECT * FROM carddb WHERE name LIKE '%"+keyword+"%'";
+		String sql = "SELECT * FROM carddb WHERE name LIKE '%" + keyword + "%'";
 
-        try (Connection conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 
-             PreparedStatement ps = conn.prepareStatement(sql);
-             ResultSet rs = ps.executeQuery()){
+		try (Connection conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+		     PreparedStatement ps = conn.prepareStatement(sql);
+		     ResultSet rs = ps.executeQuery()) {
 
-            while(rs.next()) {
-                String name = rs.getString("name");
-                String phone = rs.getString("phone");
-                String companyName = rs.getString("company_name");
-                Date createDate = rs.getDate("create_date");
+			while (rs.next()) {
+				String name = rs.getString("name");
+				String phone = rs.getString("phone");
+				String companyName = rs.getString("company_name");
+				Date createDate = rs.getDate("create_date");
 
-                businessCardList.add(new BusinessCard(name, phone, companyName, createDate));
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+				businessCardList.add(new BusinessCard(name, phone, companyName, createDate));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        return businessCardList;
-    }
+		return businessCardList;
+	}
 
-    public BusinessCard addBusinessCard(BusinessCard businessCard){
-        
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch(ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+	public BusinessCard addBusinessCard(BusinessCard businessCard) {
+
+		String sql = "INSERT INTO carddb(name,phone,company_name,create_date) VALUES(?,?,?,now());";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try (Connection conn = DriverManager.getConnection(dburl, dbUser, dbpasswd);
+		     PreparedStatement ps = conn.prepareStatement(sql)) {
+
+			ps.setString(1, businessCard.getName());
+			ps.setString(2, businessCard.getPhone().length() == 0 ? null : businessCard.getPhone());//전화번호가 없는경우
+			ps.setString(3, businessCard.getCompanyName());
+
+			ps.executeUpdate();
+		} catch (SQLIntegrityConstraintViolationException e) {
+			System.out.println("동일한 전화번호가 존재합니다. 다시 입력해 주세요");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return businessCard;
+	}
 }
