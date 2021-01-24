8f5cba585b09dfe8450c6de1d9d022b17506840d
DESKTOP-KI3R1T4\yijaeseop <vavonae@gmail.com>
2021-01-24 21:24:26 +0900

    DBUtil connection 수정완료

--- guestbook/src/main/java/org/edwith/webbe/guestbook/util/DBUtil.java
+++ guestbook/src/main/java/org/edwith/webbe/guestbook/util/DBUtil.java
@@ -3,19 +3,20 @@ package org.edwith.webbe.guestbook.util;
 import java.sql.*;
 
 public class DBUtil {
-    public static Connection getConnection(){
-        // 본인 database에 맞게끔 수정해주세요.
-        return getConnection("jdbc:mysql://localhost:3306/connectdb?useUnicode=true&characterEncoding=utf8&useSSL=false","connectuser","connect123!@#");
-    }
 
-    public static Connection getConnection(String dbURL, String dbId, String dbPassword){
-        Connection conn = null;
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection(dbURL, dbId, dbPassword);
-            return conn;
-        }catch(Exception ex){
-            throw new RuntimeException("Connection Error");
-        }
-    }
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/connectdb?useUnicode=true&characterEncoding=utf8&useSSL=false";
+	private static final String DB_USER = "connectuser";
+	private static final String DB_PASSWD = "connect123!@#";
+
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWD);
+			return conn;
+		} catch (Exception ex) {
+			throw new RuntimeException("Connection Error");
+		}
+	}
+
 }
