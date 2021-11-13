29448e01156c051a023b7d6fe36b547bd189da32
DESKTOP-QRQJMKB\jaeseop <vavonae@gmail.com>
2021-11-13 21:53:48 +0900

    DTO 클래스 생성 getter, setter ,tostring 삽입

---
+++ reservation/src/main/java/kr/or/connect/reservation/dto/ReservationUserComment.java
@@ -0,0 +1,77 @@
+package kr.or.connect.reservation.dto;
+
+import java.util.List;
+
+public class ReservationUserComment {
+	private int id;
+	private int productId;
+	private int reservationInfoId;
+	private int score;
+	private String reservationEmail;
+	private String comment;
+	private String createDate;
+	private String modifyDate;
+	private List<ReservationUserCommentImage> reservationUserCommentImages;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public int getReservationInfoId() {
+		return reservationInfoId;
+	}
+	public void setReservationInfoId(int reservationInfoId) {
+		this.reservationInfoId = reservationInfoId;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getReservationEmail() {
+		return reservationEmail;
+	}
+	public void setReservationEmail(String reservationEmail) {
+		this.reservationEmail = reservationEmail;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(String modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	public List<ReservationUserCommentImage> getReservationUserCommentImages() {
+		return reservationUserCommentImages;
+	}
+	public void setReservationUserCommentImages(List<ReservationUserCommentImage> reservationUserCommentImages) {
+		this.reservationUserCommentImages = reservationUserCommentImages;
+	}
+	@Override
+	public String toString() {
+		return "ReservationUserComment [id=" + id + ", productId=" + productId + ", reservationInfoId="
+				+ reservationInfoId + ", score=" + score + ", reservationEmail=" + reservationEmail + ", comment="
+				+ comment + ", createDate=" + createDate + ", modifyDate=" + modifyDate
+				+ ", reservationUserCommentImages=" + reservationUserCommentImages + "]";
+	}
+	
+}
