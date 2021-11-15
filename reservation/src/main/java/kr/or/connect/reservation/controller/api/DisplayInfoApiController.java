5511cc46f8bff75544b24d7189d72ee0a685fec4
DESKTOP-QRQJMKB\jaeseop <vavonae@gmail.com>
2021-11-15 17:33:11 +0900

    /api/displayinfos/{displayId}, /api/displayinfos? 구현 예외처리 등록

--- reservation/src/main/java/kr/or/connect/reservation/controller/api/DisplayInfoApiController.java
+++ reservation/src/main/java/kr/or/connect/reservation/controller/api/DisplayInfoApiController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,32 +15,75 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.or.connect.reservation.dto.Product;
+import kr.or.connect.reservation.dto.ReservationUserComment;
+import kr.or.connect.reservation.service.DisplayInfoImageService;
+import kr.or.connect.reservation.service.ProductImageService;
+import kr.or.connect.reservation.service.ProductPriceService;
 import kr.or.connect.reservation.service.ProductService;
+import kr.or.connect.reservation.service.ReservationUserCommentService;
 
 @RestController
 @RequestMapping("/api")
 public class DisplayInfoApiController {
 	@Autowired
 	private ProductService productService;
+	@Autowired
+	private ProductImageService productImageService;
+	@Autowired
+	private DisplayInfoImageService displayInfoImageService;
+	@Autowired
+	private ReservationUserCommentService reservationUserCommentService;
+	@Autowired
+	private ProductPriceService productPriceService;
 
 	@GetMapping("/displayinfos")
 	public ResponseEntity<Map<String, Object>> getDisplayInfos(
-			@RequestParam(name="categoryId", required=false, defaultValue="0") int categoryId,
-			@RequestParam(name="start", required=false, defaultValue="0") int start) {
+			@RequestParam(name = "categoryId", required = false, defaultValue = "0") int categoryId,
+			@RequestParam(name = "productId", required = false, defaultValue = "0") int productId,
+			@RequestParam(name = "start", required = false, defaultValue = "0") int start) {
+		// catgoryId와 productId 둘중 하나의 값만 올수있음
+		if (categoryId != 0 && productId != 0) {
+			return new ResponseEntity<Map<String, Object>>(HttpStatus.BAD_REQUEST);
+		}
 		Map<String, Object> map = new HashMap<>();
-		List<Product> list;
 		int totalCount;
-		if(categoryId==0) {
-			list=productService.getDisplayAllInfos(start);
-			totalCount=productService.getAllCount();
+		if (productId == 0) {
+			List<Product> list;
+			if (categoryId == 0) {
+				list = productService.getDisplayAllInfos(start);
+				totalCount = productService.getAllCount();
+			}
+			else {
+				list = productService.getDisplayInfos(categoryId, start);
+				totalCount = productService.getDisplayCount(categoryId);
+			}
+			map.put("totalCount", totalCount);
+			map.put("productCount", list.size());
+			map.put("products", list);
 		}
 		else {
-			list=productService.getDisplayInfos(categoryId, start);
-			totalCount=productService.getDisplayCount(categoryId);
+			List<ReservationUserComment> list =reservationUserCommentService.getDisplayInfos(productId, start);
+			map.put("totalCount", reservationUserCommentService.getTotalCount(productId));
+			map.put("commentCount", list.size());
+			map.put("reservationUserComments", list);
 		}
-		map.put("totalCount", totalCount);
-		map.put("productCount", list.size());
-		map.put("products", list);
+		return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+
+	}
+
+	@GetMapping("/displayinfos/{displayId}")
+	public ResponseEntity<Map<String, Object>> getDisplayInfos(@PathVariable int displayId) {
+		Map<String, Object> map = new HashMap<>();
+		try {
+			map.put("product", productService.getDisplayInfo(displayId));
+			map.put("productImages", productImageService.getDisplayInfos(displayId));
+			map.put("displayInfoImages", displayInfoImageService.getDisplayInfos(displayId));
+			map.put("avgScore", reservationUserCommentService.getAvgScore(displayId));
+			map.put("productPrices", productPriceService.getDisplayInfos(displayId));
+		} catch (RuntimeException e) {
+			return new ResponseEntity<Map<String, Object>>(HttpStatus.BAD_REQUEST);
+		}
 		return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
 	}
+
 }
