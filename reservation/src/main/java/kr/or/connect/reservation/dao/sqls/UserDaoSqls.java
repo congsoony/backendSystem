f4590ad2d1deca41be738faa011f8c809c40aa1e
DESKTOP-QRQJMKB\jaeseop <vavonae@gmail.com>
2021-11-24 02:16:41 +0900

    UserDao 추가 이메일로 user 찾기

---
+++ reservation/src/main/java/kr/or/connect/reservation/dao/sqls/UserDaoSqls.java
@@ -0,0 +1,7 @@
+package kr.or.connect.reservation.dao.sqls;
+
+public class UserDaoSqls {
+	public static final String SELECT_ALL = "select id,name,password,email,phone,create_date,modify_date \r\n"
+			+ "from user\r\n";
+	public static final String BY_EMAIL= "where email=:email";
+}
