8cf15bc405e301ccfc03f48b69a255957d44735d
DESKTOP-QRQJMKB\jaeseop <vavonae@gmail.com>
2021-11-24 01:20:45 +0900

    User, UserRole dto 생성

---
+++ reservation/src/main/java/kr/or/connect/reservation/dto/UserRole.java
@@ -0,0 +1,33 @@
+package kr.or.connect.reservation.dto;
+
+public class UserRole {
+	private Integer id;
+	private Integer userId;
+	private String roleName;
+	
+	public UserRole() {
+	}
+	public UserRole(Integer userId, String roleName) {
+		this.userId = userId;
+		this.roleName = roleName;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	
+}
