f91c2d9ba87a2282d02fc3490d4c073c3d46b5e2
DESKTOP-QRQJMKB\jaeseop <vavonae@gmail.com>
2021-11-23 23:35:10 +0900

    pjt3 api해당요청들 인증인가 무시

--- reservation/src/main/java/kr/or/connect/reservation/config/SecurityConfig.java
+++ reservation/src/main/java/kr/or/connect/reservation/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package kr.or.connect.reservation.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// 해당경로에 대한 요청은 인증/인가 처리하지 않도록 무시합니다.
+		web.ignoring().antMatchers("/api/categories","/api/displayinfos**","/api/promotions","/api/displayinfos/**");
+	}
 	
 }
