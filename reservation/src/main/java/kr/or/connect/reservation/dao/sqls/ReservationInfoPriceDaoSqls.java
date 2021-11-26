724968c307f36e5a50446b3022fd465aaaa01f67
DESKTOP-QRQJMKB\jaeseop <vavonae@gmail.com>
2021-11-26 15:03:52 +0900

    ReservationInfoprice select ,insert, delete sql생성

---
+++ reservation/src/main/java/kr/or/connect/reservation/dao/sqls/ReservationInfoPriceDaoSqls.java
@@ -0,0 +1,12 @@
+package kr.or.connect.reservation.dao.sqls;
+
+public class ReservationInfoPriceDaoSqls {
+	public static final String SELECT_RESERVATION_INFO_PRICE="select id, reservation_info_id, product_price_id,count \r\n"
+			+ "from reservation_info_price rip\r\n"
+			+ "where rip.reservation_info_id=:reservationInfoId\r\n";
+	
+	public static final String INSERT_RESERVATION_INFO_PRICE="insert into reservation_info_price(reservation_info_id,product_price_id,count)\r\n"
+			+ "values(:reservationInfoId,:productPriceId,:count)";
+	public static final String DELETE_BY_RESERVATION_INFO_ID="delete from reservation_info_price\r\n"
+			+ "where reservation_info_id=:reservationInfoId";
+}
