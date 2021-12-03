c542730fcf71914a3f95f2cc2b2fd97cc94333fa
DESKTOP-QRQJMKB\jaeseop <vavonae@gmail.com>
2021-12-04 01:20:49 +0900

    테이블 insert 추가

---
+++ reservation/src/main/java/kr/or/connect/reservation/dao/FileInfoDao.java
@@ -0,0 +1,36 @@
+package kr.or.connect.reservation.dao;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import kr.or.connect.reservation.dto.FileInfoTable;
+import static kr.or.connect.reservation.dao.sqls.FileInfoDaoSqls.*;
+
+@Repository
+public class FileInfoDao {
+	private NamedParameterJdbcTemplate jdbc;
+	private RowMapper<FileInfoTable> rowMapper = BeanPropertyRowMapper.newInstance(FileInfoTable.class);
+
+	public FileInfoDao(DataSource dataSource) {
+		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
+	}
+
+	public Integer insertFileInfo(String fileName, String saveFileName, String contentType) {
+		FileInfoTable data = new FileInfoTable();
+		data.setFileName(saveFileName);
+		data.setSaveFileName(saveFileName);
+		data.setContentType(contentType);
+		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(data);
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbc.update(INSERT_FILE_INFO, params, keyHolder);
+		return keyHolder.getKey().intValue();
+	}
+
+}
--- reservation/src/main/java/kr/or/connect/reservation/dao/ReservationUserCommentImageDao.java
+++ reservation/src/main/java/kr/or/connect/reservation/dao/ReservationUserCommentImageDao.java
@@ -1,5 +1,8 @@
 package kr.or.connect.reservation.dao;
 
+import static kr.or.connect.reservation.dao.sqls.ReservationUserCommentImageDaoSqls.BY_RESERVATION_USER_COMMENT_ID;
+import static kr.or.connect.reservation.dao.sqls.ReservationUserCommentImageDaoSqls.SELECT_ALL;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -7,10 +10,14 @@ import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import kr.or.connect.reservation.dto.ReservationUserCommentImage;
+import kr.or.connect.reservation.dto.ReservationUserCommentImageTable;
 import static kr.or.connect.reservation.dao.sqls.ReservationUserCommentImageDaoSqls.*;
 @Repository
 public class ReservationUserCommentImageDao {
@@ -21,5 +28,15 @@ public class ReservationUserCommentImageDao {
 	}
 	List<ReservationUserCommentImage> getReservationUserCommentImages(int reservationUserCommentId){
 		return jdbc.query(SELECT_ALL+BY_RESERVATION_USER_COMMENT_ID, Collections.singletonMap("reservationUserCommentId",reservationUserCommentId),rowMapper);
+	}
+	public Integer insertReservationUserCommentImage(int reservationInfoId,int reservationUserCommentId,int fileId) {
+		ReservationUserCommentImageTable data =new ReservationUserCommentImageTable();
+		data.setReservationInfoId(reservationInfoId);
+		data.setReservationUserCommentId(reservationUserCommentId);
+		data.setFileId(fileId);
+		BeanPropertySqlParameterSource params =new BeanPropertySqlParameterSource(data);
+		KeyHolder keyHolder =new GeneratedKeyHolder();
+		jdbc.update(INSERT_USER_COMMENT_IMAGE, params,keyHolder);
+		return keyHolder.getKey().intValue();
 	}
 }
