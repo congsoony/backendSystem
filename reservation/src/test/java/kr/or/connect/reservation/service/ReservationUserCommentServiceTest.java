afad2bfe8b48363081ec9a07389a0517bd8e990c
DESKTOP-QRQJMKB\jaeseop <vavonae@gmail.com>
2021-12-04 01:21:57 +0900

    파일과함께한 테스트 완료

--- reservation/src/test/java/kr/or/connect/reservation/service/ReservationUserCommentServiceTest.java
+++ reservation/src/test/java/kr/or/connect/reservation/service/ReservationUserCommentServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import kr.or.connect.reservation.config.ApplicationConfig;
+import kr.or.connect.reservation.dto.FileInfoTable;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {ApplicationConfig.class})
@@ -28,5 +29,21 @@ public class ReservationUserCommentServiceTest {
 	public void errorTest() {
 		String email="kimjinsu@connect.co.kr";
 		int productId = reservationUserCommentService.postComment(1, 3, "멋지다", email);
+	}
+	
+	@Test
+	public void addCommentAndFileTest() {
+		String email= "carami@connect.co.kr";
+		FileInfoTable fileData= new FileInfoTable();
+		String fileName = "1_map_1.png";
+		String saveFileName = "img_map/1_map_1.png";
+		String contentType= "image/png";
+		fileData.setFileName(fileName);
+		fileData.setSaveFileName(saveFileName);
+		fileData.setContentType(contentType);
+		int productId = reservationUserCommentService.addCommentAndFile(1, 3, "멋진곳이군", email, fileData);
+		
+		System.out.println("productid : "+productId);
+		System.out.println("성공");
 	}
 }
