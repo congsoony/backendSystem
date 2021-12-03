cc0734aae35fbd797516b3352bbb03b2e9a99e89
DESKTOP-QRQJMKB\jaeseop <vavonae@gmail.com>
2021-12-03 15:12:25 +0900

    date ->LocalDate로 변경

--- reservation/src/main/java/kr/or/connect/reservation/dto/ReservationUserCommentImage.java
+++ reservation/src/main/java/kr/or/connect/reservation/dto/ReservationUserCommentImage.java
@@ -1,6 +1,8 @@
 package kr.or.connect.reservation.dto;
 
 import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 public class ReservationUserCommentImage {
 	private Integer id;
 	private Integer reservationInfoId;
@@ -9,68 +11,89 @@ public class ReservationUserCommentImage {
 	private String saveFileName;
 	private String contentType;
 	private Integer deleteFlag;
-	private Date createDate;
-	private Date modifyDate;
+	private LocalDateTime createDate;
+	private LocalDateTime modifyDate;
+	
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Integer getReservationInfoId() {
 		return reservationInfoId;
 	}
+
 	public void setReservationInfoId(Integer reservationInfoId) {
 		this.reservationInfoId = reservationInfoId;
 	}
+
 	public Integer getFileId() {
 		return fileId;
 	}
+
 	public void setFileId(Integer fileId) {
 		this.fileId = fileId;
 	}
+
 	public String getFileName() {
 		return fileName;
 	}
+
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
+
 	public String getSaveFileName() {
 		return saveFileName;
 	}
+
 	public void setSaveFileName(String saveFileName) {
 		this.saveFileName = saveFileName;
 	}
+
 	public String getContentType() {
 		return contentType;
 	}
+
 	public void setContentType(String contentType) {
 		this.contentType = contentType;
 	}
+
 	public Integer getDeleteFlag() {
 		return deleteFlag;
 	}
+
 	public void setDeleteFlag(Integer deleteFlag) {
 		this.deleteFlag = deleteFlag;
 	}
-	public Date getCreateDate() {
+
+	public LocalDateTime getCreateDate() {
 		return createDate;
 	}
-	public void setCreateDate(Date createDate) {
+
+	public void setCreateDate(LocalDateTime createDate) {
 		this.createDate = createDate;
 	}
-	public Date getModifyDate() {
+
+	public LocalDateTime getModifyDate() {
 		return modifyDate;
 	}
-	public void setModifyDate(Date modifyDate) {
+
+	public void setModifyDate(LocalDateTime modifyDate) {
 		this.modifyDate = modifyDate;
 	}
+
 	@Override
 	public String toString() {
 		return "ReservationUserCommentImage [id=" + id + ", reservationInfoId=" + reservationInfoId + ", fileId="
 				+ fileId + ", fileName=" + fileName + ", saveFileName=" + saveFileName + ", contentType=" + contentType
 				+ ", deleteFlag=" + deleteFlag + ", createDate=" + createDate + ", modifyDate=" + modifyDate + "]";
 	}
+	
+	
 	
 	
 }
