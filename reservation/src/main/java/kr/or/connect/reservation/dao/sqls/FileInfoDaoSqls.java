e423be148d5e8eb64435be8bcd90889e2a2fc714
DESKTOP-QRQJMKB\jaeseop <vavonae@gmail.com>
2021-12-05 13:35:21 +0900

    file sql 작성 , user_comment  where 절 생략문 작성

--- reservation/src/main/java/kr/or/connect/reservation/dao/sqls/FileInfoDaoSqls.java
+++ reservation/src/main/java/kr/or/connect/reservation/dao/sqls/FileInfoDaoSqls.java
@@ -1,7 +1,9 @@
 package kr.or.connect.reservation.dao.sqls;
 
 public class FileInfoDaoSqls {
-	public static final String INSERT_FILE_INFO="insert into file_info(file_name,save_file_name,content_type,delete_flag,create_date,modify_date)\r\n"
+	public static final String INSERT_FILE_INFO = "insert into file_info(file_name,save_file_name,content_type,delete_flag,create_date,modify_date)\r\n"
 			+ "values(:fileName,:saveFileName,:contentType,0,now(),now())";
-	
+	public static final String SELECT_ALL = "select id,file_name,save_file_name,content_type,delete_flag,create_date,modify_date \r\n"
+			+ "from file_info\r\n";
+	public static final String BY_ID = "where id = :id";
 }
--- reservation/src/main/java/kr/or/connect/reservation/dao/sqls/ReservationUserCommentDaoSqls.java
+++ reservation/src/main/java/kr/or/connect/reservation/dao/sqls/ReservationUserCommentDaoSqls.java
@@ -6,12 +6,13 @@ public class ReservationUserCommentDaoSqls {
 	public static final String BY_DISPLAY_INFO_ID ="where di.id = :displayInfoId";
 	
 	public static final String SELECT_USER_COMMENT = "select ruc.id as id, ruc.product_id as product_id, ri.id as reservation_info_id,\r\n"
-			+ "ruc.score as score,u.email as reservation_email , ruc.comment as comment, ruc.create_date as create_date, ruc.modify_date as modify_date\r\n"
+			+ "ruc.score as score,u.email as reservation_email , ruc.comment as comment,u.id as user_id, ruc.create_date as create_date, ruc.modify_date as modify_date\r\n"
 			+ "from reservation_user_comment ruc \r\n"
 			+ "inner join reservation_info ri on ri.id = ruc.reservation_info_id\r\n"
 			+ "inner join user u on u.id = ruc.user_id\r\n";
 	public static final String LIMIT_BY_PRODUCT_ID = "where ruc.product_id =:productId\r\n"
 			+ "order by ruc.id desc limit :start,:limit";
+	public static final String ORDER_LIMIT="order by ruc.id desc limit :start,:limit";
 	
 	public static final String SELECT_TOTAL_COUNT="select count(*) as total_count\r\n"
 			+ "from reservation_user_comment ruc \r\n"
